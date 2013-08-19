8ff02d043b2db3e0311f6e57c8dab3cb63156936
ChangSeop.Kim <infini2753@gmail.com>
2013-08-19 11:31:47 +0900

    t

--- build_server/jenkins_config/_Batch_Script_Jenkins/_Base/_Date - 복사본.bat
+++ build_server/jenkins_config/_Batch_Script_Jenkins/_Base/_Date - 복사본.bat
@@ -14,6 +14,8 @@ SET T=%HOUR%%TIME:~3,2%%TIME:~6,2%
 SET D=%DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2%
 rem	SET D=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%
 
+mkdir %D%
+
 rem SET DATETIME=%D%_%T%
 
 SET DATETIME=%D%
